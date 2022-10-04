7e4e9beffd00e6df942ddc59b295c056ed5cc05e
ElonFusl <115004317+ElonFusl@users.noreply.github.com>
2022-10-04 14:22:55 +0200

    Update code.sh

--- code.sh
+++ code.sh
@@ -2,6 +2,6 @@
 
 argument1=$1
 argument2=$((argument1 + 20)) 
-parallel --will-cite -j 2 php -f gap_direct_insert_archiveteam.php {} ::: {"10$1","11$1"} &
+parallel --will-cite -j 2 php -f gap_direct_insert_archiveteam.php {} ::: {"$argument1","$argument2"} &
 wait
 echo "All Complete"
